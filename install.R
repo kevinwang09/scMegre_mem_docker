ddd88d2c32111c85d05bae34e37a1c694361ddbe
Kevin Wang <kevin.wang@sydney.edu.au>
2019-09-27 13:37:00 +1000

    initialise gcbuild with shiny

--- install.R
+++ install.R
@@ -5,6 +5,6 @@ BiocManager::install(c("SingleCellExperiment", "DelayedArray",
                        "Matrix", "TENxPBMCData",
                        "scater", "M3Drop", 
                        "ruv", "devtools",
-                       "hadley/lineprof"), version = "3.10", update = TRUE, ask = FALSE)
+                       "hadley/lineprof", "shiny"), version = "3.10", update = TRUE, ask = FALSE)
 
 BiocManager::install("SydneyBioX/scMerge", ref = "master")
